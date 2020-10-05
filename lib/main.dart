8fd556ddfffe20b6bdfc8483ee05bbe2c4ead6a3
Farid Hidayat <e.faridhidayat@gmail.com>
2020-10-06 00:52:55 +0700

    move widgets to inside of class

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Quizzy',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Quizzy'),
@@ -34,65 +22,60 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-var questionTextBox = Container(
-    padding: EdgeInsets.all(20),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'One Piece Quiz',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: 20),
-        Text(
-          'The first question for you is? bla bla bla bla',
-          style: TextStyle(fontSize: 20),
-        ),
-      ],
-    ));
-
-var answerChoicesBox = Expanded(
-    flex: 8,
-    child: Column(
-      children: <Widget>[
-        ListTile(
-          title: Text(
-              'Multiline answer Multiline answer Multiline answer Multiline answer Multiline answer Multiline answer',
-              style: TextStyle(fontSize: 16)),
-          leading: Radio(
-            value: 'a',
-            groupValue: 'anu',
-            onChanged: (value) {},
-          ),
-        ),
-        ListTile(
-          title: Text('Thomas Jefferson', style: TextStyle(fontSize: 16)),
-          leading: Radio(
-            value: 'b',
-            groupValue: 'anu',
-            onChanged: (value) {},
-          ),
-        ),
-      ],
-    ));
-
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+
+  Container questionTextBox(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'One Piece Quiz',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'The first question for you is? bla bla bla bla',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ));
+  }
+
+  Expanded answerChoicesBox(BuildContext context) {
+    return Expanded(
+        flex: 8,
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(
+                  'Multiline answer Multiline answer Multiline answer Multiline answer Multiline answer Multiline answer',
+                  style: TextStyle(fontSize: 16)),
+              leading: Radio(
+                value: 'a',
+                groupValue: 'anu',
+                onChanged: (value) {},
+              ),
+            ),
+            ListTile(
+              title: Text('Thomas Jefferson', style: TextStyle(fontSize: 16)),
+              leading: Radio(
+                value: 'b',
+                groupValue: 'anu',
+                onChanged: (value) {},
+              ),
+            ),
+          ],
+        ));
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -124,8 +107,8 @@ class _MyHomePageState extends State<MyHomePage> {
         // in the middle of the parent.
         child: Column(
           children: <Widget>[
-            questionTextBox,
-            answerChoicesBox,
+            questionTextBox(context),
+            answerChoicesBox(context),
             // Row(
             //   children: <Widget>[
             //   Text(
